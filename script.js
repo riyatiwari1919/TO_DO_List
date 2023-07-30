4abe246b148237cce5bfa035733ad407a6cd10c7
riyatiwari1919 <98682707+riyatiwari1919@users.noreply.github.com>
2023-07-30 13:24:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+function makeAnimation(value){
+    document.querySelector("." + value).classList.add("clicked");
+
+    setTimeout(function(){
+        document.querySelector("." + value).classList.remove("clicked");
+    },100);
+}
+
+function createUl(){
+    var newUl = document.createElement("ul");
+    newUl.classList.add("task-list");
+    document.querySelector(".task-box").appendChild(newUl);
+}
+
+function addTask(){
+        
+    var enteredTask = document.querySelector(".input").value;
+    
+    if (enteredTask == ""){
+            alert("Enter Task");
+    }
+    else{
+        console.log("Added task is : " + enteredTask);
+
+        var newLi = document.createElement("li");
+        newLi.classList.add("task-list-li");
+        document.querySelector(".task-list").appendChild(newLi);
+
+        var newLiCheckbox = document.createElement("input");
+        newLiCheckbox.type = "checkbox";
+        newLiCheckbox.classList.add("checkbox");
+        newLi.appendChild(newLiCheckbox);
+        
+        var newLiDiv = document.createElement("div");
+        newLiDiv.classList.add("task-text");
+        newLi.appendChild(newLiDiv);
+        newLiDiv.innerHTML = enteredTask;
+
+        var newLiBtn = document.createElement("button");
+        newLiBtn.classList.add("cross-btn");
+        newLi.appendChild(newLiBtn);
+        newLiBtn.innerHTML = "X";
+
+        newLiCheckbox.addEventListener("click", function(){
+            newLiDiv.classList.toggle("done");
+            console.log("Task completed : " + newLiDiv.innerHTML);
+        });
+
+        var totalLibtns = document.querySelectorAll(".cross-btn").length;
+        for(var i=0; i<totalLibtns; i++){
+            document.querySelectorAll(".cross-btn")[i].addEventListener("click", function(){
+                var removedTask = this.parentElement.firstChild.nextElementSibling.innerHTML;
+                console.log("Removed task is : " + removedTask);
+                this.parentElement.remove();
+            });
+        }
+        document.querySelector(".input").value = "";
+    }
+}
+
+document.querySelector(".add-btn").addEventListener("click", function(){
+    makeAnimation("add-btn");
+    addTask();    
+});
+
+document.querySelector(".input").addEventListener("keypress", function(event){
+    if (event.key === "Enter"){
+        makeAnimation("add-btn");
+        addTask();
+    }
+});
+
+document.querySelector(".clear-btn").addEventListener("click", function(){
+    if (document.querySelectorAll("li").length == 0){
+        alert("Enter Task");
+    }
+    else{
+        makeAnimation("clear-btn");
+        console.log(document.querySelector(".task-list"));
+        document.querySelector(".task-list").remove();
+        console.log("All tasks Cleared!");
+        createUl(); 
+    }
+});
